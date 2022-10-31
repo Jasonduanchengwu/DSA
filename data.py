2a4d3fa86a04a4bdddac8be99f2d7427d206d8c9
jasonduanchengwu <duanchengwu001@gmail.com>
2022-10-30 17:08:41 -0700

    added array

--- data.py
+++ data.py
@@ -1,1 +1,17 @@
-print("hello")
+class data:
+    def __init__(self) -> None:
+        self.array = [0]*3
+
+    def create_array(self):
+        for i in range(3):
+            self.array[i]=i
+        return self.array
+
+def main():
+    struc = data()
+    array = struc.create_array()
+    print(array)
+
+if __name__ == "__main__":
+    main()
+    
