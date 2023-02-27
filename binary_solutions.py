dff25948943052d79701614caeae65c3030b23aa
jasonduanchengwu <duanchengwu001@gmail.com>
2023-02-26 16:35:00 -0800

    min_bit_flips

--- binary_solutions.py
+++ binary_solutions.py
@@ -26,6 +26,30 @@ class b_solutions:
         # l.append(b)
         # return sum(l)
 
+    def min_bit_flips(self, start: int, goal: int) -> int:
+        """
+            Descriptions:   Given start, determine the minimum bit flips to get to the goal
+            start:          int
+            goal:           int
+            return:         minimum bit flips
+        """
+        # str of binary start and goal
+        s,g = bin(start)[2:],bin(goal)[2:]
+        # Trade MC for TC
+        s_len, g_len = len(s), len(g)
+        
+        # if length does not match, append 0 at the front abs difference about of times
+        if s_len != g_len:
+            if s_len>g_len:
+                g="0"*(s_len-g_len)+g
+            else:
+                s="0"*(g_len-s_len)+s
+        flips = 0
+        for i, ele in enumerate(s):
+            if ele is not g[i]:
+                flips+=1
+        return flips
+    
 if __name__ == "__main__":
     b=b_solutions()
     print(b.two_int_sum(3,4))
