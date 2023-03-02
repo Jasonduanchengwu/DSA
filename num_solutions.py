d4ef3103d5d4f31936c3f6b22243d69cc15ab31b
jasonduanchengwu <duanchengwu001@gmail.com>
2023-03-02 15:51:29 -0800

    added docstrings and common factor

--- num_solutions.py
+++ num_solutions.py
@@ -1,5 +1,13 @@
 class num_solutions:
     def num_of_matches(self, n: int)->int:
+        """
+            Descriptions:   find the number of matches played with n number of competitors
+                            (Even n competitors will be paired two each and 
+                            the winner advances to the next rounds, odd n competitors will have 1 person advance randomly
+                            the rest of the even pairs compete)
+            n:              number of competitors
+            return:         num of matches
+        """
         match = 0
         while n > 1:
             if n%2 == 1:
@@ -10,6 +18,19 @@ class num_solutions:
                 n //=2
         return match
 
+    def num_of_common_factors(self, a: int, b: int) -> int:
+        """
+            Descriptions:   find the number of common factors of a and b
+            a,b:            two int
+            return:         the number
+        """
+        def smaller_int(a: int, b: int) -> int:
+            if a>=b:
+                return b
+            else: return a
+        # trading slightly lower TC for readability
+        return len([i for i in range(1,smaller_int(a,b)+1) if a%i==0 and b%i==0])
+    
 if __name__ == "__main__":
     no = num_solutions()
     n = 7
